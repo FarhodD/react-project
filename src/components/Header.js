f7d015fd7858cb4c2be5248b5465f162af0b31e1
FarhodD <farhoddavlatov0120@gmail.com>
2023-05-02 16:23:55 +0500

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img className='logo' src="https://e7.pngegg.com/pngimages/615/494/png-clipart-logo-design-text-symbol.png" alt="Логотип" />
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='menu'>
+        <a href="#ere" className="menu__item">Profiles</a>
+        <a href="#ef" className="menu__item">Messages</a>
+        <a href="#rtrt" className="menu__item">News</a>
+        <a href="#trt" className="menu__item">Music</a>
+        <a href="#drer" className="menu__item">Settings</a>
+      </nav>
+  )
+}
+
+export default Navbar;
