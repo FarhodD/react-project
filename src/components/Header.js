99a3aa0cecc93203817badfe5c5c02df5309f60c
FarhodD <farhoddavlatov0120@gmail.com>
2023-05-02 17:42:52 +0500

    css-modules

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import h from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className='header'>
-      <img className='logo' src="https://e7.pngegg.com/pngimages/615/494/png-clipart-logo-design-text-symbol.png" alt="Логотип" />
+    <header className= {h.header}>
+      <img className={h.logo} src="https://e7.pngegg.com/pngimages/615/494/png-clipart-logo-design-text-symbol.png" alt="Логотип" />
     </header>
   )
 }
