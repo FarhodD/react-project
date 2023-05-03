21ef52431bb2cb0d077a01ff8ac4f1439abf7c80
FarhodD <farhoddavlatov0120@gmail.com>
2023-05-03 22:39:07 +0500

    NavLink

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile';
+import { Route, Routes } from 'react-router-dom';
 
 
 
@@ -13,9 +14,11 @@ const App = () => {
       <Header />
       <Navbar />
       <div className="app__content">
-        <Dialogs />
+        <Routes>
+          <Route path='/dialogs' element={<Dialogs />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
       </div>
-      {/* <Profile /> */}
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import n from './Navbar.module.css';
 
 const Navbar = () => {
   return (
     <nav className={n.menu}>
-        <a href="#ere" className={n.menu__item}>Profiles</a>
-        <a href="#ef" className={n.menu__item}>Messages</a>
+        <NavLink to="/profile" className={n.menu__item}>Profiles</NavLink>
+        <NavLink to="/dialogs" className={n.menu__item}>Messages</NavLink>
         <a href="#rtrt" className={n.menu__item}>News</a>
         <a href="#trt" className={n.menu__item}>Music</a>
         <a href="#drer" className={n.menu__item}>Settings</a>
