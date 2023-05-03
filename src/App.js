a40e2f51ee48de5a65ab3796e216186a9e1b920a
FarhodD <farhoddavlatov0120@gmail.com>
2023-05-03 18:54:20 +0500

    dialogs

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
+import Dialogs from './components/Dialogs/Dialogs';
 import Profile from './components/Profile';
+
 
 
 const App = () => {
@@ -10,7 +12,10 @@ const App = () => {
     <div className='app__wrapper'>
       <Header />
       <Navbar />
-      <Profile />
+      <div className="app__content">
+        <Dialogs />
+      </div>
+      {/* <Profile /> */}
     </div>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './Navbar.css';
+import n from './Navbar.module.css';
 
 const Navbar = () => {
   return (
-    <nav className='menu'>
-        <a href="#ere" className="menu__item">Profiles</a>
-        <a href="#ef" className="menu__item">Messages</a>
-        <a href="#rtrt" className="menu__item">News</a>
-        <a href="#trt" className="menu__item">Music</a>
-        <a href="#drer" className="menu__item">Settings</a>
+    <nav className={n.menu}>
+        <a href="#ere" className={n.menu__item}>Profiles</a>
+        <a href="#ef" className={n.menu__item}>Messages</a>
+        <a href="#rtrt" className={n.menu__item}>News</a>
+        <a href="#trt" className={n.menu__item}>Music</a>
+        <a href="#drer" className={n.menu__item}>Settings</a>
       </nav>
   )
 }
