bfc4f3ac166431e481b00a5a135d74ec56d4742a
FarhodD <farhoddavlatov0120@gmail.com>
2023-08-03 10:17:40 +0500

    compose

--- src/components/Dialogs/DialogsContainer.js
+++ src/components/Dialogs/DialogsContainer.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { compose } from 'redux';
 import { WithAuthRedirect } from '../../hoc/WithAuthRedirect';
 import { sendMessageCreator, updateNewMessageBodyCreator } from '../../redux/dialogsReducer';
 import Dialogs from './Dialogs';
 
-let AuthRedirectComponent = WithAuthRedirect(Dialogs)
 
-let mapStateToProps = (state) => {
-  return {
-    dialogsPage: state.dialogsPage,
-    isAuth: state.auth.isAuth
-  }
-}
 
 let mapDispatchToProps = (dispatch) => {
   return {
@@ -25,7 +19,14 @@ let mapDispatchToProps = (dispatch) => {
 }
 
 
+let mapStateToProps = (state) => {
+  return {
+    dialogsPage: state.dialogsPage,
+    isAuth: state.auth.isAuth
+  }
+}
 
-const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(AuthRedirectComponent)
-
-export default DialogsContainer;
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  WithAuthRedirect
+)(Dialogs);
